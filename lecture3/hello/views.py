d8f059c92ae2b16b227f097595a877b2ca899fae
Abhimanyu Sharma <abhisea12@gmail.com>
2022-02-23 19:15:16 -0800

    Day 2 of Django

--- lecture3/hello/views.py
+++ lecture3/hello/views.py
@@ -4,4 +4,15 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello ")
+    return render(request, "hello/index.html")
+
+def abhi(request):
+    return HttpResponse("Hello Abhi")
+
+def david(request):
+    return HttpResponse("hello david")
+
+def greet(request, name):
+    return render(request, "hello/greet.html", {
+        "name": name.capitalize()
+    })
