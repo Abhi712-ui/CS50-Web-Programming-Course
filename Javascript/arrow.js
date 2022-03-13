2615fce232309272b816afdb15dcdd90351bdcb5
Abhimanyu Sharma <abhisea12@gmail.com>
2022-03-13 12:23:55 -0700

    More Javascript!

---
+++ Javascript/arrow.js
@@ -0,0 +1,8 @@
+button => {
+        //This is another way of creating a function in javascript, called the arrow function
+    }
+    //if a function took in no inputs, you could just do this
+
+() => {
+
+}
